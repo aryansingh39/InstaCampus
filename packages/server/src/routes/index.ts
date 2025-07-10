ae2f2745714942d5784ded788e87cb14344bd707
Amit Sharma <amittsharma4567@gmail.com>
2025-07-10 20:15:00 +0530

    Update index.ts

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -4,15 +4,12 @@ import postRoutes from './postRoutes';
 import commentRoutes from './commentRoutes';
 import reactionRoutes from './reactionRoutes';
 
-
-
-
-
 const router = Router();
+
+router.use('/auth', authRoutes);
+router.use('/posts', postRoutes);
 router.use('/', commentRoutes); 
 router.use('/', reactionRoutes);  
-router.use('/posts', postRoutes);
-router.use('/auth', authRoutes);
 
 router.get('/hello', (req, res) => {
   res.json({ message: 'Hello InstaCampus Backend!' });
